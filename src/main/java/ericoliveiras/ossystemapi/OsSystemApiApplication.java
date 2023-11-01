5af80fa59af9ff4a41835a8e61efdcb49cbdf853
Eric Oliveira <oiericoi@hotmail.com>
2023-11-01 20:39:01 -0300

    build: init project

---
+++ src/main/java/ericoliveiras/ossystemapi/OsSystemApiApplication.java
@@ -0,0 +1,11 @@
+package ericoliveiras.ossystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSystemApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OsSystemApiApplication.class, args);
+	}
+}
---
+++ src/test/java/ericoliveiras/ossystemapi/OsSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ericoliveiras.ossystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
