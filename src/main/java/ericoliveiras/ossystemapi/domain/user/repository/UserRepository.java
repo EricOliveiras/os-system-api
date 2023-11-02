ee5047eef5ad6ac7d73f33ba5ef2f0f7eb467889
Eric Oliveira <oiericoi@hotmail.com>
2023-11-02 13:29:29 -0300

    feat: add user repository

---
+++ src/main/java/ericoliveiras/ossystemapi/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ericoliveiras.ossystemapi.domain.user.repository;
+
+import ericoliveiras.ossystemapi.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
